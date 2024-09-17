b0e5c2b7b903262339701e1882b5027c527232a4
Alan Ensina <alanvinicius.ce@gmail.com>
2024-09-17 10:33:36 +0100

    Remove references

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { InvestmentCreateComponent } from './components/investments/investment-create/investment-create.component';
 import { InvestmentListComponent } from './components/investments/investment-list/investment-list.component';
-import { CashControlCreateComponent } from './components/cash-control/cash-control-create/cash-control-create.component';
 import { CashControlListComponent } from './components/cash-control/cash-control-list/cash-control-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
@@ -13,9 +11,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', component: LoginComponent },
   { path: 'new-user', component: NewUserComponent },
   { path: 'home', component: MainMenuComponent },
-  { path: 'investments/new', component: InvestmentCreateComponent },
   { path: 'home/investments', component: InvestmentListComponent },
-  { path: 'cash-control/new', component: CashControlCreateComponent },
   { path: 'home/cash-control', component: CashControlListComponent },
   { path: 'home/dashboard', component: DashboardComponent },
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,9 +18,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewUserComponent } from './components/new-user/new-user.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { CashControlCreateComponent } from './components/cash-control/cash-control-create/cash-control-create.component';
 import { InvestmentListComponent } from './components/investments/investment-list/investment-list.component';
-import { InvestmentCreateComponent } from './components/investments/investment-create/investment-create.component';
 import { CashControlListComponent } from './components/cash-control/cash-control-list/cash-control-list.component';
 import { CustomSidenavComponent } from './components/core/custom-sidenav/custom-sidenav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -31,10 +29,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     LoginComponent,
     NewUserComponent,
     MainMenuComponent,
-    CashControlCreateComponent,
     CashControlListComponent,
     InvestmentListComponent,
-    InvestmentCreateComponent,
     CustomSidenavComponent,
     DashboardComponent
   ],
